9a01b93978b138b0211f3b58dd80f7ffe28986ba
SACS1 <sealcaseescuela@gmail.com>
2022-11-28 16:05:22 -0600

    AdminItem

---
+++ src/Components/AdminItem/AdminItem.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './AdministradorItem.scss';
+
+const AdministradorItem =()=>{
+    return(
+        <>
+            <div className="item">
+                <span className="text-item"></span>
+            </div>
+        </>
+    );
+}
+
+export default Dashboard;
