54d7dfcf857010d292efdadef3bc1e56392ecd32
SACS1 <sealcaseescuela@gmail.com>
2022-12-20 22:30:44 -0600

    Preguntas

--- src/Components/Formulario/Formulario.jsx
+++ src/Components/Formulario/Formulario.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import NavBar3 from "../../Common/NavBar3/NavBar3";
 import './Formulario.scss';
 import Footer from "../../Common/Footer/Footer";
+import Pregunta from "./Pregunta/Pregunta";
 const Formulario=()=>{
     return(
         <>  
             <NavBar3/>
+            <div className="container-seccion">
 
+            </div>
+            <div className="container-formulario">
+                <Pregunta/>
+                <Pregunta/>
+                <Pregunta/>
+                <Pregunta/>
+            </div>
             <Footer/>
         </>
     );
